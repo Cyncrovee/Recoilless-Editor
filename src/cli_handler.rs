9df38ba30256c075856f994ec361c086fca7ec10
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-01-15 22:47:29 +0000

    feat: add new CLI arguments to show key bindings

--- src/cli_handler.rs
+++ src/cli_handler.rs
@@ -7,8 +7,11 @@ pub fn boot_arg() {
     match args.get(1) {
         Some(_) => {
             match args[1].as_str() {
-                "-h" | "--help"=> {
+                "-h" | "--help" => {
                     show_help();
+                }
+                "-k" | "--keys" => {
+                    show_keybinds();
                 }
                 _ => {
                     // Pass
@@ -91,6 +94,47 @@ fn show_help() {
     println!("      Replace filepath with the full path, name, and extension of the file you are trying to open if applicable");
     println!("");
     println!("You can also open a file by simply adding the file name/extension as the first flag. Both this and -n/--name work based off the current working directory, so you can also input a file path from that directory.");
+    println!("------------------------------------------------------------------------");
+    std::process::exit(0);
+}
+
+fn show_keybinds() {
+    println!("------------------------------------------------------------------------");
+    println!("");
+    println!("Below is an overview of some of the keybinds for the editor (not all are included here, but this should cover most of the commonly used ones).");
+    println!("There are some duplicates (i.e two keybinds that do the same thing), just due to the built-in keybinds in tui-textarea that I'm not sure if I want to remove. However the differences between edit mode and insert mode should make this less of a problem.");
+    println!("");
+    println!("Modes:");
+    println!("- i: Switch to insert mode (when in edit mode)");
+    println!("- Esc: Switch to edit mode (when in insert mode)");
+    println!("");
+    println!("Movement (Edit Mode)");
+    println!("- hjkl/arrow keys: Move left, down, up and right");
+    println!("- Ctrl + w: Move forward by word");
+    println!("- Alt + w: Move backward by word");
+    println!("- Ctrl + l: Move forward by line");
+    println!("- Alt + l: Move backward by line");
+    println!("- Ctrl + e: Jump to start of line");
+    println!("- Alt + e: Jump to end of line");
+    println!("- Ctrl + p: Jump back by paragraph");
+    println!("- Alt + p: Jump forward by paragraph");
+    println!("- Ctrl + j: Jump to start of file");
+    println!("- Alt + j: Jump to end of file");
+    println!("");
+    println!("Editing (Edit Mode)");
+    println!("- Ctrl + Alt + w: Delete word");
+    println!("- Ctrl + Alt + l: Delete line");
+    println!("- Ctrl + n :Make a new line below current line");
+    println!("- Alt + n :Make a new line above current line");
+    println!("- u: Undo");
+    println!("- r: Redo");
+    println!("- p: Paste");
+    println!("");
+    println!("Editor (Edit Mode)");
+    println!("- Ctrl + home: Exit program");
+    println!("- Ctrl + s: Save file");
+    println!("- Ctrl + Alt + s: Save file and exit program");
+    println!("");
     println!("------------------------------------------------------------------------");
     std::process::exit(0);
 }
